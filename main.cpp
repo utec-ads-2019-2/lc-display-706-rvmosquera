55f3e114fb3f9e0e0ca004bc9790ffe693ab9a03
Raúl Mosquera <raulv.mosquera@gmail.com>
2019-10-30 21:05:04 -0500

    accepted :v

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void printstr(string str, int length) {
 
 int main() {
     int s;
-    unsigned long long int n;
+    string n;
     int rows,cols;
 
     string template_row[10];
@@ -20,7 +20,6 @@ int main() {
     string vertical_bar_l[10]; //left
     string vertical_bar_r[10]; //right
     string blank_row[10];
-    string line;
 
     for(int i=1 ; i <= 10 ; ++i) {
         int j=0;
@@ -50,7 +49,7 @@ int main() {
     }
 
     while(cin>>s>>n) {
-        auto res = s + n;
+        auto res = s + stoi(n);
 
         if (!res)
             break;
@@ -58,7 +57,7 @@ int main() {
         rows = 2 * s + 3;
         auto mid_row = s + 1;
         cols = s + 2;
-        auto num = to_string(n);
+        auto num = n;
 
         for(int i=0 ; i < rows ; ++i) {
             for(auto it=num.begin() ; it != num.end(); ++it) {
@@ -66,114 +65,87 @@ int main() {
                 if( i == 0 ) {
                     switch(*it) {
                         case '1':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
-                            //cout << blank_row[s-1]; break;
                             printstr( blank_row[s-1], cols); break;
                         case '4':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
-                            //cout << blank_row[s-1]; break;
                             printstr( blank_row[s-1], cols); break;
                         default:
-                            //printf("%s", template_row[s-1].c_str() );
                             printstr(template_row[s-1], cols);
                     }
 
-                    if (it != num.end())
+                    if (it != num.end()- 1)
                         printf(" ");
 
                 } else if( i == mid_row) {
                     switch(*it) {
                         case '0':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
                             printstr(blank_row[s-1], cols ); break;
                         case '1':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
                             printstr( blank_row[s-1], cols ); break;
                         case '7':
-                           // printf("%s", blank_row[s-1].c_str() ); break;
                             printstr( blank_row[s-1], cols ); break;
                         default:
-                            //printf("%s", template_row[s-1].c_str() );
-                            //printstr(template_row[s-1], cols);
                             printstr(template_row[s-1], cols);
                     }
-                    if (it != num.end())
+                    if (it != num.end() - 1)
                         printf(" ");
 
                 } else if ( i == rows - 1) {
                     switch(*it) {
                         case '1':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
                             printstr( blank_row[s-1], cols ); break;
                         case '4':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
                             printstr(blank_row[s-1], cols ); break;
                         case '7':
-                            //printf("%s", blank_row[s-1].c_str() ); break;
                             printstr(blank_row[s-1], cols ); break;
                         default:
-                            //printf("%s", template_row[s-1].c_str() );
                             printstr(template_row[s-1], cols);
                     }
 
-                    if (it != num.end())
+                    if (it != num.end()- 1)
                         printf(" ");
                 } else {
                     switch(*it) {
                         case '1':
-                            //printf("%s", vertical_bar_r[s - 1].c_str() ); break;
                             printstr(vertical_bar_r[s - 1], cols ); break;
                         case '2':
                             if ( i < mid_row)
-                                //printf("%s", vertical_bar_r[s - 1].c_str() );
                                 printstr(vertical_bar_r[s - 1], cols);
                             else
-                                //printf("%s", vertical_bar_l[s - 1].c_str() );
                                 printstr( vertical_bar_l[s - 1], cols  );
                             break;
                         case '3':
                             printf("%s", vertical_bar_r[s - 1].c_str() ); break;
                         case '4':
                             if ( i < mid_row)
-                                //printf("%s", vertical_bar[s - 1].c_str() );
                                 printstr(vertical_bar[s - 1], cols );
                             else
-                                //printf("%s", vertical_bar_r[s - 1].c_str() );
                                 printstr(vertical_bar_r[s - 1], cols );
                             break;
                         case '5':
                             if ( i < mid_row)
-                                //printf("%s", vertical_bar_l[s - 1].c_str() );
                                 printstr(vertical_bar_l[s - 1], cols );
                             else
-                                //printf("%s", vertical_bar_r[s - 1].c_str() );
                                 printstr(vertical_bar_r[s - 1], cols );
                             break;
                         case '6':
                             if ( i < mid_row)
-                                //printf("%s", vertical_bar_l[s - 1].c_str() );
                                 printstr( vertical_bar_l[s - 1], cols );
                             else
-                                //printf("%s", vertical_bar[s - 1].c_str() );
                                 printstr( vertical_bar[s - 1], cols );
                             break;
                         case '7':
-                            //printf("%s", vertical_bar_r[s - 1].c_str() ); break;
                             printstr(vertical_bar_r[s - 1], cols ); break;
                         case '9':
                             if ( i < mid_row)
-                                //printf("%s", vertical_bar[s - 1].c_str() );
                                 printstr(vertical_bar[s - 1], cols );
                             else
-                                //printf("%s", vertical_bar_r[s - 1].c_str() );
                                 printstr(vertical_bar_r[s - 1], cols );
                             break;
                         default:
-                            //printf("%s", vertical_bar[s - 1].c_str() );
                             printstr( vertical_bar[s - 1], cols );
                     }
 
-                    if (it != num.end())
+                    if (it != num.end()- 1)
                         printf(" ");
                 }
 
